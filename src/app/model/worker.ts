e5947b5e75710216688002eaf7f47edf30a2a489
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-09 10:45:24 +0200

    add worker model

---
+++ src/app/model/worker.ts
@@ -0,0 +1,10 @@
+export interface Worker {
+  id: number;
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  age: number;
+  email: string;
+  photo: string;
+  description: string;
+}
