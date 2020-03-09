52c345de179ba664d2fdc16eb0310b6a1559c87b
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-09 10:44:42 +0200

    add product model

---
+++ src/app/model/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  price: number;
+  quantity: number;
+  productTypeId: number;
+  name: string;
+  description: string;
+  photo: string;
+  brand: string;
+  model: string;
+}
