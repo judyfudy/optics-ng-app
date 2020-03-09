7ea6d97a3d4d3ea6c84179d2e4931f6e5b8db0be
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-09 11:00:06 +0200

    add user component

---
+++ src/app/component/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../service/user.service";
+import {User} from "../../model/user";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  public user: User;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getAuthenticatedUserInfo().subscribe(data=> this.user = data);
+  }
+}
