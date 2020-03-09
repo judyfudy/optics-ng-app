bf313137a5ba100c977778f627a6655a3129fbcc
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-09 10:40:53 +0200

    add all necessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProductTypeComponent} from './component/productType/productType.component';
+import {SignInComponent} from './component/authentication/sign-in/sign-in.component';
+import {NavbarComponent} from './component/navbar/navbar.component';
+import {ProductComponent} from './component/product/product.component';
+import {SignUpComponent} from './component/authentication/sign-up/sign-up.component';
+import {CartComponent} from './component/cart/cart.component';
+import {WorkerComponent} from './component/worker/worker.component';
+import {AuthInterceptor} from "./service/AuthInterceptor";
+import { UserComponent } from './component/user/user.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material/material.module";
+import { HomeComponent } from './component/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductTypeComponent,
+    SignInComponent,
+    NavbarComponent,
+    ProductComponent,
+    SignUpComponent,
+    CartComponent,
+    WorkerComponent,
+    UserComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
