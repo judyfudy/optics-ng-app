5c1d4aabae401a78aaba29cfc70345e03f1bd922
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-09 10:42:16 +0200

    add cart.service.ts

---
+++ src/app/service/cart.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpEvent, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Product} from "../model/product";
+import {Observable} from "rxjs";
+import {ProductType} from "../model/productType";
+
+@Injectable({providedIn: 'root'})
+export class CartService {
+
+  public getUrl: string = 'http://localhost:8080/cart/all';
+  public postUrl: string = 'http://localhost:8080/cart';
+  public clearUrl: string = 'http://localhost:8080/cart/clear';
+  public orderUrl: string = 'http://localhost:8080/order';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllProductsInCart() {
+    return this.http.get<Product[]>(this.getUrl);
+  }
+
+  addToCart(productId) {
+    this.http.post(this.postUrl, null,
+      {params: new HttpParams().set('productId', productId)}).subscribe();
+  }
+
+  deleteFromCart(productId) {
+    this.http.delete(this.postUrl,
+      {params: new HttpParams().set('productId', productId)}).subscribe();
+  }
+
+  clearCart() {
+    this.http.delete(this.clearUrl).subscribe();
+  }
+
+  getOrderAndSendEmail() {
+    this.http.post(this.orderUrl, null).subscribe();
+  }
+}
