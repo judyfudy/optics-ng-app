78f11fe2e083477f546f46c88902deecc4b8da32
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-09 10:41:07 +0200

    add all necessary routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ProductTypeComponent} from "./component/productType/productType.component";
+import {SignInComponent} from "./component/authentication/sign-in/sign-in.component";
+import {ProductComponent} from "./component/product/product.component";
+import {SignUpComponent} from "./component/authentication/sign-up/sign-up.component";
+import {CartComponent} from "./component/cart/cart.component";
+import {WorkerComponent} from "./component/worker/worker.component";
+import {UserComponent} from "./component/user/user.component";
+import {HomeComponent} from "./component/home/home.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'productType', component: ProductTypeComponent},
+  {path: 'productType/:id', component: ProductComponent},
+  {path: 'authenticate', component: SignInComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'worker', component: WorkerComponent},
+  {path: 'profile', component: UserComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
