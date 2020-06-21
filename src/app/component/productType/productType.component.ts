a3337dfa80cbb996ac58cceaa51d0da4f0594a1f
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-06-21 21:47:54 +0300

    optimize imports

--- src/app/component/productType/productType.component.ts
+++ src/app/component/productType/productType.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductType} from "../../model/productType";
 import {ProductTypeService} from "../../service/productType.service";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-product',
+  selector: 'app-productType',
   templateUrl: './productType.component.html',
   styleUrls: ['./productType.component.css']
 })
 export class ProductTypeComponent implements OnInit {
 
-  productTypes : ProductType[];
+  productTypes: ProductType[];
 
-  constructor(private router: Router, private productService: ProductTypeService) { }
+  constructor(private router: Router, private productService: ProductTypeService) {
+  }
 
   ngOnInit(): void {
     this.productService.getAllProductTypes().subscribe(data => this.productTypes = data);
   }
 
   onSelect(productType) {
-    this.router.navigate(['/productType', productType.id])
+    this.router.navigate(['/productType', productType.id]);
   }
-
 }
