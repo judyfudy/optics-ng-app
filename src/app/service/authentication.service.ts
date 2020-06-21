991c93c30cb987c6a0aa50b56b6ccacaedfe8f98
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-06-21 21:49:25 +0300

    add method to be able to login with facebook

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {map} from 'rxjs/operators';
 import {User} from "../model/user";
 
@@ -8,6 +8,7 @@ export class AuthenticationService {
 
   signInUrl: string = 'http://localhost:8080/authenticate';
   signUpUrl: string = 'http://localhost:8080/signup';
+  facebookUrl: string = 'http://localhost:8080/signinFacebook';
 
   constructor(private http: HttpClient) {
   }
@@ -26,13 +27,32 @@ export class AuthenticationService {
       }));
   }
 
+  loginFacebook(userData) {
+    return this.http.post<any>(this.facebookUrl, {
+      authToken: userData.authToken,
+      email: userData.email,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      id: userData.id,
+      name: userData.name,
+      provider: userData.provider
+    })
+      .pipe(map(jwt => {
+        if (jwt) {
+          localStorage.setItem('accessToken', `${jwt.accessToken}`);
+          localStorage.setItem('refreshToken', `${jwt.refreshToken}`);
+        }
+        return jwt;
+      }));
+  }
+
   logout() {
     // remove user from local storage to log user out
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
     let url = location.href.replace(location.origin, "");
 
-    if (location.href.substring(23,29) !== "authenticate"){
+    if (location.href.substring(23, 29) !== "authenticate") {
       window.location.href = "/authenticate?returnUrl=" + url;
     }
   }
@@ -40,4 +60,5 @@ export class AuthenticationService {
   register(user: User) {
     return this.http.post(this.signUpUrl, user);
   }
+
 }
